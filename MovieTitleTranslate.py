55fa05fe13cf212e88835a60d72579909d455aee
xu4nch3n <ccccx1594@gmail.com>
2022-08-18 15:03:18 +0800

    add MovieTitleTranslate.py, rename movie dir from Eng to ZH-CN

--- MovieTitleTranslate.py
+++ MovieTitleTranslate.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+from unittest.result import failfast
 import xml.etree.ElementTree as ET
 import sys, os
 
 
 startDir = sys.argv[1]
 print(startDir)
+failedList = []
 movies = os.listdir(startDir)
 for m in movies:
   title = ''
@@ -31,10 +33,17 @@ for m in movies:
   except:
     print("rename <%s> failed!..." % m)
     print('==========================')
+    failedList.append(mPath)
     continue
   print('rename successfully!!!!!!!!')
   print('==========================')
 
+if len(failedList) > 0 :
+  print('\n\nThe list of renamed failed:')
+  for m in failedList:
+    print(m)
+else:
+  print('\n\nAll movie dir renamed Finished!!')
 
 
     
